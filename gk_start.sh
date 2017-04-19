3fba84d707beebc587b055188431e9c5ebf7e7ac
fireraccoon <willyjames.music@gmail.com>
2017-04-18 22:32:28 -0400

    Initial commit

---
+++ gk_start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This script will run when STARTING the project ""
+# Here you might want to cd into your project directory, activate virtualenvs, etc.
+
+# The currently active project is available via $GK_ACTIVE_PROJECT
+
---
+++ gk_stop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This script will run when STOPPING the project ""
+# Here you might want to deactivate virtualenvs, clean up temporary files, etc.
+
+# The currently active project is available via $GK_ACTIVE_PROJECT
+
