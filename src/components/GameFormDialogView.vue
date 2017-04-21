9c3fb300579e69b7dbbe5db9e5250892afb84933
fireraccoon <willyjames.music@gmail.com>
2017-04-21 03:27:31 -0400

    Fixed new game modal footer

--- src/components/GameFormDialogView.vue
+++ src/components/GameFormDialogView.vue
@@ -1,75 +1,67 @@
 <template>
-<div>
- <p>dialogVisible: {{ dialogVisible }}</p>
   <el-dialog title="Create new game" v-model="dialogVisible">
     <div id="game-form" class="modal-container">
-      <div class="modal-header">
-        <form id="login-form" v-on:submit.prevent="createGame">
-          <!-- title -->
-          <div class="form-group">
-              <div class="form-error-block" v-if="errors.title">
-                  <p class="form-error" v-for="error in errors.title">{{ error }}</p>
-              </div>
-              <el-input
-                  v-model="fields.title"
-                  type="text"
-                  name="title"
-                  placeholder="Enter title"
-                  required>
-              </el-input>
-          </div>
-          <!-- description -->
-          <div class="form-group">
-              <div class="form-error-block" v-if="errors.description">
-                  <p class="form-error" v-for="error in errors.description">{{ error }}</p>
-              </div>
-              <el-input
-                  type="textarea"
-                  v-model="fields.description"
-                  name="description"
-                  placeholder="Enter description"
-                  required>
-              </el-input>
-          </div>
-          <!-- start date -->
-          <div class="form-group">
-              <div class="form-error-block" v-if="errors.start_date">
-                  <p class="form-error" v-for="error in errors.start_date">{{ error }}</p>
-              </div>
+      <form id="login-form" v-on:submit.prevent="createGame">
+        <!-- title -->
+        <div class="form-group">
+            <div class="form-error-block" v-if="errors.title">
+                <p class="form-error" v-for="error in errors.title">{{ error }}</p>
+            </div>
+            <el-input
+                v-model="fields.title"
+                type="text"
+                name="title"
+                placeholder="Enter title"
+                required>
+            </el-input>
+        </div>
+        <!-- description -->
+        <div class="form-group">
+            <div class="form-error-block" v-if="errors.description">
+                <p class="form-error" v-for="error in errors.description">{{ error }}</p>
+            </div>
+            <el-input
+                type="textarea"
+                v-model="fields.description"
+                name="description"
+                placeholder="Enter description"
+                required>
+            </el-input>
+        </div>
+        <!-- start date -->
+        <div class="form-group">
+            <div class="form-error-block" v-if="errors.start_date">
+                <p class="form-error" v-for="error in errors.start_date">{{ error }}</p>
+            </div>
 
-              <el-date-picker
-                    v-model="fields.start_date"
-                    type="datetime"
-                    format="yyyy/MM/dd HH:mm"
-                    placeholder="Select start date and time"
-                    :picker-options="pickerOptions">
-              </el-date-picker>
-          </div>
-          <!-- end date -->
-          <div class="form-group">
-              <div class="form-error-block" v-if="errors.end_date">
-                  <p class="form-error" v-for="error in errors.end_date">{{ error }}</p>
-              </div>
-              <el-date-picker
-                    v-model="fields.end_date"
-                    type="datetime"
-                    format="yyyy/MM/dd HH:mm"
-                    placeholder="Select start date and time"
-                    :picker-options="pickerOptions">
-              </el-date-picker>
-          </div>
-        </form>
-      </div> <!-- .modal-header -->
-      <div>
-        <br>
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="dialogVisible = false">Cancel</el-button>
-          <el-button type="primary" @click="createGame">Create</el-button>
-        </span>
-      </div>
+            <el-date-picker
+                  v-model="fields.start_date"
+                  type="datetime"
+                  format="yyyy/MM/dd HH:mm"
+                  placeholder="Select start date and time"
+                  :picker-options="pickerOptions">
+            </el-date-picker>
+        </div>
+        <!-- end date -->
+        <div class="form-group">
+            <div class="form-error-block" v-if="errors.end_date">
+                <p class="form-error" v-for="error in errors.end_date">{{ error }}</p>
+            </div>
+            <el-date-picker
+                  v-model="fields.end_date"
+                  type="datetime"
+                  format="yyyy/MM/dd HH:mm"
+                  placeholder="Select start date and time"
+                  :picker-options="pickerOptions">
+            </el-date-picker>
+        </div>
+      </form>
     </div>
+    <span slot="footer" class="dialog-footer">
+      <el-button type="primary" @click="createGame">Create</el-button>
+      <el-button @click="dialogVisible = false">Cancel</el-button>
+    </span>
   </el-dialog>
-</div>
 </template>
 
 <script>
