d176cbace08648fb7018b56670e85dd8f4cdc8eb
fireraccoon <willyjames.music@gmail.com>
2017-09-29 18:28:17 -0400

    Fixed 'Cannot GET /' error

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     // SS_TG
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
