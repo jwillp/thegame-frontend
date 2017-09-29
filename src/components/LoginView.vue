1eedbb5a0e82c50f23e0179d3f0d411e85ee35f3
fireraccoon <willyjames.music@gmail.com>
2017-09-29 14:44:39 -0400

    Closes #40 - Intercept responses for 401 + Better error handling axios

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -84,17 +84,33 @@ export default {
 
                 },
                 // ERROR CALLBACK
-                function(response) {
-                    console.log(response)
+                function(error) {
 
                     self.loginInProgress = false
 
-                    if(response.status == 0 || response.status == 500){
+                    if(error.status == 0 || error.status == 500){
                         self.errors = ['Server Error, please try again later']
                         return;
                     }
 
                     self.errors = ['Invalid username or password']
+
+                    if (error.response) {
+                      // The request was made and the server responded with a status code
+                      // that falls out of the range of 2xx
+                      console.log(error.response.data);
+                      console.log(error.response.status);
+                      console.log(error.response.headers);
+                    } else if (error.request) {
+                      // The request was made but no response was received
+                      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                      // http.ClientRequest in node.js
+                      console.log(error.request);
+                    } else {
+                      // Something happened in setting up the request that triggered an Error
+                      console.log('Error', error.message);
+                    }
+                    console.log(error.config);
                 },
 
                 function(response) {}
--- src/components/RegisterView.vue
+++ src/components/RegisterView.vue
@@ -117,17 +117,35 @@ export default {
                     self.registration_success = true
                     self.registrationInProgress = false
                 },
-                // ERROR CALLBACK
-                function(response) {
-                    console.log(response)
-                    self.registrationInProgress = false
-                    self.errors = response.data.errors || {}
 
-                    if(response.status == 500) {
+                // ERROR CALLBACK
+                function(error) {
+                    console.log(error)
+                    self.registrationInProgress = false
+                    self.errors = error.data.errors || {}
+
+                    if(error.status == 500) {
                         self.errors = {
                             global: ['Server Error, please try again later']
                         }
                     }
+
+                    if (error.response) {
+                      // The request was made and the server responded with a status code
+                      // that falls out of the range of 2xx
+                      console.log(error.response.data);
+                      console.log(error.response.status);
+                      console.log(error.response.headers);
+                    } else if (error.request) {
+                      // The request was made but no response was received
+                      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                      // http.ClientRequest in node.js
+                      console.log(error.request);
+                    } else {
+                      // Something happened in setting up the request that triggered an Error
+                      console.log('Error', error.message);
+                    }
+                    console.log(error.config);
                 }
             )
         }
